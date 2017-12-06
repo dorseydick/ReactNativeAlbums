4b759b30866f17d4f8d4e4780ab2232738c826b2
Dorsey Dick <dorseydick@gmail.com>
2017-12-06 07:21:09 -0500

    Initial commit

---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+//Import libraries for making registerComponent
+import React from 'react';
+import { Text } from 'react-native';
+//make a registerComponent
+
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//make component available to other parts of the app
+export default Header;
