9edb0c83dd1110c9d879c4fcf94fa9e08cab2410
Dorsey Dick <dorseydick@gmail.com>
2017-12-11 21:26:00 -0500

    Rename Header
    Rename Header

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -13,17 +13,22 @@ class AlbumList extends Component {
 
   componentDidMount() {
     console.log('componentDidMount in AlbumList');
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => response.json())
+        .then(data => this.setState({ albums: data }));
+    /*
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(
       response => this.setState({ albums: response.data })
-    );
+    );*/
   }
 
   renderAlbums() {
-    return this.state.albums.map(
-      //album => <AlbumDetail key={album.title} album={album} />
-      album => <Text key={album.title}>{album.title}</Text>
+    const list = this.state.albums.map(
+      album => <AlbumDetail key={album.title} album={album} />
+      //album => <Text key={album.title}>{album.title}</Text>
     );
+    return list;
   }
 
   render() {
