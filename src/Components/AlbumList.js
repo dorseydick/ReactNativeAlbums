bb9009c443d34ad25994722a2529beef3efd714e
Dorsey Dick <dorseydick@gmail.com>
2017-12-11 21:40:16 -0500

    Fix errors
    Fix errors with AlbumList

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,8 +1,8 @@
 //Import libraries for making registerComponent
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumList';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -13,20 +13,15 @@ class AlbumList extends Component {
 
   componentDidMount() {
     console.log('componentDidMount in AlbumList');
-    fetch('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => response.json())
-        .then(data => this.setState({ albums: data }));
-    /*
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(
       response => this.setState({ albums: response.data })
-    );*/
+    );
   }
 
   renderAlbums() {
     const list = this.state.albums.map(
       album => <AlbumDetail key={album.title} album={album} />
-      //album => <Text key={album.title}>{album.title}</Text>
     );
     return list;
   }
