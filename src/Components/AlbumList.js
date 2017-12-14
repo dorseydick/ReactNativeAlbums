18c134f92498f78608620b50966e894618d379e5
Dorsey Dick <dorseydick@gmail.com>
2017-12-13 20:34:11 -0500

    Add Card and CardSection

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -12,7 +12,6 @@ class AlbumList extends Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount in AlbumList');
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(
       response => this.setState({ albums: response.data })
