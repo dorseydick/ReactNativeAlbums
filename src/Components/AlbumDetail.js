5689e22a433ff996758eab7f2dd2dae4df90bf9f
Dorsey Dick <dorseydick@gmail.com>
2017-12-18 19:12:56 -0500

    Add Button and Press
    Add the button component and add on an onPress prop to handle user interaction.

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const {
@@ -33,11 +34,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           source={{ uri: image }}
           style={imageStyle}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
